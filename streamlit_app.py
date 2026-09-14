f6a161623169f5e62f00307a7818ea0cf18cf985
Gleison11 <53097455+Gleison11@users.noreply.github.com>
2026-09-14 12:24:15 +0000

    Atualiza CSV usado pelo app

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ st.info("Observação: lojas com três dígitos devem adicionar o L maiúsculo n
 # CARREGAR A PLANILHA
 # ==============================
 
-arquivo = Path(__file__).parent / "Atualização3.csv"
+arquivo = Path(__file__).parent / "Atualização6.csv"
 
 
 @st.cache_data
